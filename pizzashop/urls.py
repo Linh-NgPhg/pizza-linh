4ce914e7bf1f533484864ee09dfa8f38f22c45b8
Linh-NgPhg <linhphuongnguyen.lun06nd@gmail.com>
2026-09-12 19:58:56 +0700

    fix media urls

--- pizzashop/urls.py
+++ pizzashop/urls.py
@@ -3,19 +3,12 @@ URL configuration for pizzashop project.
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pizzaweb.urls")),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
-
-# Phục vụ ảnh trong thư mục media
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
